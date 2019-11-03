e1f18c7704165b3aa6e6a32010573d9dd5cb9166
pkalhori <pkalhori@g.ucla.edu>
2019-11-03 15:58:22 -0800

    adding new script

--- SLIM/R_scripts/Windows_Format/plotting_from_table.R
+++ SLIM/R_scripts/Windows_Format/plotting_from_table.R
@@ -3,35 +3,57 @@ todaysdate=format(Sys.Date(),format="%Y%m%d")
 
 
 data.dir="C:\\Users\\poone\\OneDrive\\Documents\\Otter_Exome_Project\\SLIM_results\\"
-popModDate=c("CA_AK\\2D.3Epoch.Translocation\\20191012\\")
+popModDate=c("CA_AK\\2D.3Epoch.Translocation\\20191023\\")
 
-allLoads<- read.table(paste(data.dir,popModDate,"BurnInData2D.txt",sep = ""), header = T)
+allLoads<- read.table(paste(data.dir,popModDate,"migs_data.txt",sep = ""), header = T)
 
-pop_1 <- allLoads[allLoads$subpopulation==1,]
-pop_2 <- pop_2[pop_2$generation>8000,]
-pop_2 <- allLoads[allLoads$subpopulation==2,]
+
 # label H:
 
-pop_2$hLabel <- paste("h = ",pop_2$h)
-pop_2$model<- "2D.3Epoch.Translocation"
-pop_2$population <- factor(pop_2$population,levels=c("CA_AK"))
+allLoads$hLabel <- paste("h = ",allLoads$h)
+allLoads <- allLoads[allLoads$generation>=8000,]
+allLoads$subpopulation[allLoads$subpopulation==1] <- "AK"
+allLoads$subpopulation[allLoads$subpopulation==2] <- "CA"
+
+
+
 p2 <- 
-  ggplot(pop_2,aes(x=generation,y=L_mutationLoad))+
+  ggplot(allLoads,aes(x=generation,y=L_mutationLoad, colour=migLabel))+
   #geom_point(position=position_dodge(.5),size = .1,alpha=0.5)+
-  stat_summary(fun.y = "mean", geom = "point", color = "red", size = 1)+
-  stat_summary(fun.y = "mean", geom = "line", color = "black", size = 0.5)+
+  #stat_summary(fun.y = "mean", geom = "point", size = 1, color=model)+
+  stat_summary(fun.y = "mean", geom = "line", size = 0.5)+
   theme_bw()+
-  facet_grid(hLabel~interaction(pop_1$model),scales="free")+
+  facet_grid(hLabel~interaction(allLoads$subpopulation),scales="free")+
   ylab("Genetic Load")+
   xlab("Generation") +
-  theme(legend.position = "none")+
-  ggtitle("Genetic Load for 3 Epoch Model")+
-  geom_vline(xintercept=8038,color="blue",size=0.5)+
-  geom_vline(xintercept=8056,color="blue",size=0.5)+
-  geom_vline(xintercept=8012,color="blue",size=0.5)
+  theme(legend.position = "left")+
+  ggtitle("Genetic Load for 3 Epoch Model With Translocation")+
+  geom_vline(data=dates, aes(xintercept=value))+
+  stat_summary(fun.data = mean_sdl, geom = "errorbar")+
+  theme(legend.title=element_blank())
+              
+p2head(allLoads)
+allLoads$migLabel <- NA
+allLoads[allLoads$model=="2D.3Epoch.Translocation.1perGen",]$migLabel <- "1 ind/gen"
+allLoads[allLoads$model=="2D.3Epoch.Translocation.10perGen",]$migLabel <- "10 ind/gen"
+allLoads[allLoads$model=="2D.3Epoch.Translocation.5perGen",]$migLabel <- "5 ind/gen"
+allLoads[allLoads$model=="2D.3Epoch.Translocation.25perGen",]$migLabel <- "25 ind/gen"
+dates$
+AK <- c(8002,8038,8056)
+CA <- c(8012,8038,8056)
+library(reshape2)
+dates <- melt(rbind(AK,CA))
+library(tidyverse)
+dates <- dates %>% group_by(Var1)
 
 plot.dir="C:\\Users\\poone\\OneDrive\\Documents\\Otter_Exome_Project\\SLIM_results\\genetic_load_calcs\\"
-ggsave(paste(plot.dir,"Load.2D.CA",todaysdate,".pdf",sep=""),p2,height=6,width=8)
+ggsave(paste(plot.dir,"Multiple_Migs",todaysdate,".pdf",sep=""),p2,height=6,width=8)
+
+library(reshape2)
+
+
+
+
 max(allLoads$L_mutationLoad[allLoads$h==0])
 min(allLoads$L_mutationLoad[allLoads$h==0])
 mean(allLoads$L_mutationLoad[allLoads$h==0 & allLoads$generation==96])
