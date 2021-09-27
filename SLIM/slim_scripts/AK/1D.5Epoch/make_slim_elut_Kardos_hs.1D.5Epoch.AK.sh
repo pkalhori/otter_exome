36fba284d36ce426e25f99a02e633e643149becb
pkalhori <pkalhori@g.ucla.edu>
2021-09-27 11:55:38 -0700

    adding lethal muts to slim scripts

--- SLIM/slim_scripts/AK/1D.5Epoch/make_slim_elut_Kardos_hs.1D.5Epoch.AK.sh
+++ SLIM/slim_scripts/AK/1D.5Epoch/make_slim_elut_Kardos_hs.1D.5Epoch.AK.sh
@@ -1,3 +1,51 @@
+# Script to make SLIM job script
+# USAGE: ./make_slim_wolf_101517.job.sh [h]
+pop=AK
+model=1D.5Epoch
+#model=1D.2Epoch.1.5Mb.cds.LongerContract
+gitdir=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/github_repos/otter_exome/SLIM
+scriptdir=$gitdir/slim_scripts/$pop/$model
+mkdir -p $scriptdir # set script dir
+todaysdate=`date +%Y%m%d`
+########## population specific parameters ########
+# set sample size for vcf (should match empirical for AK)
+# can set manually or pull from a table
+ss=7 # in diploids # will depend on population, note you can find these in projectionValues.txt file
+#population	ProjectionValueHaploids Diploids
+#KUR	12	6
+#CA	12	6
+#COM	34	17
+#AK	14	7
+#AL	20	10
+# variables:
+nanc=4500 # a ball park Nanc for AK
+nu=250 # contraction size  -- a medium contraction size
+tcontract=35 # contraction duration before you sample -- a longer contraction time than dadi ifnerence (more like FSC)
+trecovery=13 #1911-1989 is around 13 generations (6y/gen) 
+nrec=2500 #not sure what population size to use; best I could find for WPWS
+tspill=5 ##want to model a very short populatoin crash
+nspill=250 #around 90% of otters in the area were killed
+ntoday=2500 #assuming complete recovery 
+
+# this nu / T combo is within the MLE estimate for Alaska from my grid search. also want to try the California version (100 for 30 gens with smaller Nanc of ~3500)
+######### general parameters ; can set here or in command line ##########
+# Set g, number of genes (exons)
+g=1000
+# Set gLen, the length of exons
+gLen=1500
+# Set t, number of burn-in generations
+t=50000
+# set mutation rate
+mu=8.64e-9 # mutation rate
+# Set h, dominance coefficient
+h=$1  # loop through hs
+# Set j, the chunk number (for 14 chunks?)
+
+# Make script
+# chunk gets set when you run slim (based on SGE task id) # or something?? how to do this part? I don't really want to make a separte slim script each time? have it be a -d thing maybe?
+# have to figure out the chunks/replicates situation.
+
+cat > $scriptdir/slim_elut_${model}_${pop}_DengLynch_hs.job << EOM
 
 // changes to make: apparently 1e-03 is reasonable between-gene recomb rate
 // and then want to make separate chromosomes with 0.5 between them (or just simulate them separately)
@@ -5,32 +53,35 @@
 // recomb w/in each gene will be 1e-08, between genes will be 1e-3
 // will either do 14x1.5mb so they are independent, or will simulate them all together if want to think about overall genetic load (then would have to set up chromosomes within the simulation)
 initialize() {
-	defineConstant("g",1000); //number of genes; starting with 1000 (AB)
-	defineConstant("geneLength", 1500); // length of each gene
+	defineConstant("g",$g); //number of genes; starting with 1000 (AB)
+	defineConstant("geneLength", $gLen); // length of each gene
 	defineConstant("seqLength", g*geneLength); // total sequence length starting with 1.5Mb (AB)
-	//defineConstant("outdir",\"\"); -- set in command line
-	//defineConstant("v_CHUNK",); // portion of genome  -- set in command line
-	//defineConstant("v_REP",); // overall replicate number -- set in command line
-	defineConstant("v_h",0.5); // dominance coefficient
-	defineConstant("v_SS",7); // sample size
-	defineConstant("v_MU",8.64e-9);
-	defineConstant("v_NANC",4500); // ancestral size
-	defineConstant("v_NU",250); // contraction size
-	defineConstant("v_NREC",2500); // contraction size
-	defineConstant("v_NSPILL",250); // size after spill
-	defineConstant("v_NTODAY",2500); // today's population 
+	//defineConstant("outdir",\"$outdir\"); -- set in command line
+	//defineConstant("v_CHUNK",$chunk); // portion of genome  -- set in command line
+	//defineConstant("v_REP",$rep); // overall replicate number -- set in command line
+	defineConstant("v_h",$h); // dominance coefficient
+	defineConstant("v_SS",$ss); // sample size
+	defineConstant("v_MU",$mu);
+	defineConstant("v_NANC",$nanc); // ancestral size
+	defineConstant("v_NU",$nu); // contraction size
+	defineConstant("v_NREC",$nrec); // contraction size
+	defineConstant("v_NSPILL",$nspill); // size after spill
+	defineConstant("v_NTODAY",$ntoday); // today's population 
 	
 	//cat("Exome portion length:"+seqLength+"\n");
 	initializeMutationRate(v_MU);
 	// m1 mutation type: neutral *synonymous*
 	initializeMutationType("m1", 0.5, "f", 0.0);
 	// m2 mutation type: missense(?) -- This is from Chris; ask where he got params (Kim et al?)
-	initializeMutationType("m2", v_h, "g",-0.01314833, 0.186); // set H in array (0 for recessive, 0.5 for additive) 
+	initializeMutationType("m2", 0.5, "g",-0.05, 0.5); // set H in array (0 for recessive, 0.5 for additive) 
+	// m3 mutation type: lethal mutations
+	initializeMutationType("m3", 0.0, "f", -1.0);
+	m3.convertToSubstitution = F; 
 	m2.convertToSubstitution = F; // okay cool if you have this, then fixed sites are in the vcf file
 	m1.convertToSubstitution = F; // keeps fixed sites in vcf file (do I want?)
 	// initialize exon: g1, has both neutral (m1) and misense (m2) mutations
 	// syn happens at rate 1, mis at rate 2.31:1 since there are more missense sites (Christian/Chris); note chris names neutral as m2 and missense at m1 so I reversed things here 
-	initializeGenomicElementType("g1", c(m1,m2), c(1.0,2.31)); // 2.31 is the NS:S ratio
+	initializeGenomicElementType("g1", c(m1,m2,m3), c(43.3,95,5)); // this keeps the 2.31:1 NS:S ratio with 5% lethal
 	
 	for (i in 0:(g-1)){
 		initializeGenomicElement(g1, ((i)*geneLength+1), ((i+1)*geneLength));
@@ -54,8 +105,8 @@ initialize() {
 }
 
 1: fitness(m2) {
-h = (0.5)/(1 - 7071.07*(mut.selectionCoeff));
-//h = mut.mutationType.dominanceCoeff;
+// this is from Deng and Lynch: 
+h = 0.5 * exp(-13*abs(mut.selectionCoeff));
 if (homozygous) {
     // 20210107: this was Bernard's code to deal with slight excess heterosis in his Plos Genet paper; we don't need this; initial set of revisions was run with this, but then we re-ran without it for final submission --> return ((1.0 + 0.5*mut.selectionCoeff)*(1.0 + 0.5*mut.selectionCoeff));
     // 20210107: now am calculating homozgyous derived fitness the same as in the other simulations: 
@@ -72,16 +123,16 @@ if (homozygous) {
 
 // output generation number so I can track progress
 
-1:50000 late() {
+1:${t} late() {
 	if (sim.generation % 1000 == 0){
 		cat(sim.generation+"\n");
 	}
 }
 //After burn in, calculate load every 2 generations
-50000 late() {
+${t} late() {
 	writeFile(paste(c(outdir,"/slim.output.",v_CHUNK,".summary.txt"),sep=""),"replicate,chunk,generation,mutid,type,s,age,originpop,subpop,numhet,numhom,popsizeDIP\n",append=F); // open fresh file
 }
-50000: late() {
+${t}: late() {
 	if (sim.generation % 2 == 0){
 	//file header
 	//mutation id
@@ -126,6 +177,8 @@ if (homozygous) {
 				type = "m1";
 			} else if (type == m2){
 				type = "m2";
+			} else if (type == m3){
+				type = "m3";
 			}
 			//print results
 		writeFile(paste(c(outdir,"/slim.output.",v_CHUNK,".summary.txt"),sep=""),paste(c(v_REP,v_CHUNK,generation,id,type,s,age,originpop,popID,pnumhet,pnumhom,popsize),sep=","),append=T);
@@ -136,47 +189,48 @@ if (homozygous) {
 
 // after t generation burn in, sample individuals and output counts across whole population as well (from JAR script) ;
 // then do this again after the contraction -- then only need to simulate once instead of doing 1 and 2 epoch separately. 
-50000 late() {
+${t} late() {
 	p1.outputVCFSample(v_SS, F,filePath=paste(c(outdir,"/slim.output.PreContraction.",v_CHUNK,".vcf"),sep=""));
 
 }
 
 // contract the population 1 gen after burn in:
-50001 {
+$((${t} + 1)) {
 	p1.setSubpopulationSize(v_NU);
 	}
 // Then keep it contracted for an additional +tContract
-50036 late() {
+$((${t} + 1+ ${tcontract})) late() {
 	p1.outputVCFSample(v_SS, F,filePath=paste(c(outdir,"/slim.output.PostContraction.",v_CHUNK,".vcf"),sep=""));
 
 }
 // expand the population 1 generation after sampling:
-50037 {
+$((${t} + 2 + ${tcontract})) {
 	p1.setSubpopulationSize(v_NREC);
 	}
 // Proceed for trecovery until the present day, then sample
-50050 late() {
+$((${t} + 2+ ${tcontract}+${trecovery})) late() {
 	p1.outputVCFSample(v_SS, F,filePath=paste(c(outdir,"/slim.output.PostRecovery.",v_CHUNK,".vcf"),sep=""));
 
 }
 // contract the population again generation after sampling:
-50051 {
+$((${t} + 3+ ${tcontract}+${trecovery})) {
 	p1.setSubpopulationSize(v_NSPILL);
 	}
 // Proceed for trecovery until the present day, then sample
-50056 late() {
+$((${t} + 3+ ${tcontract}+${trecovery}+${tspill})) late() {
 	p1.outputVCFSample(v_SS, F,filePath=paste(c(outdir,"/slim.output.PostSpill.",v_CHUNK,".vcf"),sep=""));
 
 }
 // recover the populatoin to original size:
-50057 {
+$((${t} + 4+ ${tcontract}+${trecovery}+${tspill})) {
 	p1.setSubpopulationSize(v_NTODAY);
 	}
 // sample every generation
 
-50057:50107 late() {
+$((${t} + 4 + ${tcontract} + ${trecovery} + ${tspill})):$((${t} + 54 + ${tcontract} + ${trecovery} + ${tspill})) late() {
 	p1.outputVCFSample(v_SS, F,filePath=paste(c(outdir,"/slim.output.SpillRecovery.",sim.generation,"gen.",v_CHUNK,".vcf"),sep=""));
 
 }
-50108 late() { sim.outputFull(); } 
+$((${t} + 55+ ${tcontract}+${trecovery}+${tspill})) late() { sim.outputFull(); } 
 
+EOM
--- SLIM/slim_scripts/CA_AK/2D.3Epoch.Translocation.1perGen/make_slim_elut_Kardos_hs.2D.3Epoch.Translocation.1perGen.CA_AK.sh
+++ SLIM/slim_scripts/CA_AK/2D.3Epoch.Translocation.1perGen/make_slim_elut_Kardos_hs.2D.3Epoch.Translocation.1perGen.CA_AK.sh
@@ -1,3 +1,62 @@
+pop=CA_AK
+model=2D.3Epoch.Translocation.1perGen
+#model=1D.2Epoch.1.5Mb.cds.LongerContract
+gitdir=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/github_repos/otter_exome/SLIM
+scriptdir=$gitdir/slim_scripts/$pop/$model
+mkdir -p $scriptdir # set script dir
+todaysdate=`date +%Y%m%d`
+########## population specific parameters ########
+# set sample size for vcf (should match empirical for AK)
+# can set manually or pull from a table
+ss_CA=6
+ss_AK=7 # in diploids # will depend on population, note you can find these in projectionValues.txt file
+#population	ProjectionValueHaploids Diploids
+#KUR	12	6
+#CA	12	6
+#COM	34	17
+#AK	14	7
+#AL	20	10
+# variables:
+nanc_combo=4500
+nanc_AK=4500 # a ball park Nanc for AK
+nu_AK=250
+nanc_CA=3500
+nu_CA=200
+ # contraction size  -- a medium contraction size
+tcontract=35 # contraction duration before you sample -- a longer contraction time than dadi ifnerence (more like FSC)
+trecovery=17 #1911-2019 is around 17 generations (6y/gen)
+tfuture=50 #allow simulation to continue running at recovered pop size  
+nrec_AK=2500 #not sure what population size to use; best I could find for WPWSx
+nrec_CA=1000
+# this nu / T combo is within the MLE estimate for Alaska from my grid search. also want to try the California version (100 for 30 gens with smaller Nanc of ~3500)
+######### general parameters ; can set here or in command line ##########
+
+
+migAtoC=1e-3
+migCtoA=4e-4
+
+# Set g, number of genes (exons)
+g=1000
+# Set gLen, the length of exons
+gLen=1500
+# Set t, number of burn-in generations
+t=50000
+tdiv=4000
+# set mutation rate
+mu=8.64e-9 # mutation rate
+# Set h, dominance coefficient
+h=$1  # loop through hs
+# Set j, the chunk number (for 14 chunks?)
+
+chr=20
+#total number of chromosomes, since all "chunks" will be simulated together 
+
+
+# Make script
+# chunk gets set when you run slim (based on SGE task id) # or something?? how to do this part? I don't really want to make a separte slim script each time? have it be a -d thing maybe?
+# have to figure out the chunks/replicates situation.
+
+cat > $scriptdir/slim_elut_${model}_${pop}_DengLynch_hs.job << EOM
 
 // changes to make: apparently 1e-03 is reasonable between-gene recomb rate
 // and then want to make separate chromosomes with 0.5 between them (or just simulate them separately)
@@ -5,25 +64,25 @@
 // recomb w/in each gene will be 1e-08, between genes will be 1e-3
 // will either do 14x1.5mb so they are independent, or will simulate them all together if want to think about overall genetic load (then would have to set up chromosomes within the simulation)
 initialize() {
-	defineConstant("g",1000); //number of genes; starting with 1000 (AB)
-	defineConstant("geneLength", 1500); // length of each gene
+	defineConstant("g",$g); //number of genes; starting with 1000 (AB)
+	defineConstant("geneLength", $gLen); // length of each gene
 	defineConstant("seqLength", g*geneLength); // total chromosome length starting with 1.5Mb (AB)
-	//defineConstant("outdir",\"\"); -- set in command line
-	//defineConstant("v_CHUNK",); // portion of genome  -- set in command line
-	//defineConstant("v_REP",); // overall replicate number -- set in command line
-	defineConstant("v_h",0.5); // dominance coefficient
-	defineConstant("v_SS_CA",6); // sample size
-	defineConstant("v_MU",8.64e-9);
-	defineConstant("v_NANC_CA",3500); // ancestral size for CA
-	defineConstant("v_NU_CA",200); // contraction size California
-	defineConstant("v_NREC_CA",1000); // recovery size CA
-	defineConstant("v_SS_AK",7); // sample size
-	defineConstant("v_NANC_AK",4500); // ancestral size
-	defineConstant("v_NU_AK",250); // contraction size
-	defineConstant("v_NREC_AK",2500); // contraction size
-	defineConstant("v_NCOMBO",4500); // pre split time
-	//defineConstant("migAK_CA",); // migration to CA from AK
-	//defineConstant("migCA_AK",); // migration to AK from CA
+	//defineConstant("outdir",\"$outdir\"); -- set in command line
+	//defineConstant("v_CHUNK",$chunk); // portion of genome  -- set in command line
+	//defineConstant("v_REP",$rep); // overall replicate number -- set in command line
+	defineConstant("v_h",$h); // dominance coefficient
+	defineConstant("v_SS_CA",$ss_CA); // sample size
+	defineConstant("v_MU",$mu);
+	defineConstant("v_NANC_CA",$nanc_CA); // ancestral size for CA
+	defineConstant("v_NU_CA",$nu_CA); // contraction size California
+	defineConstant("v_NREC_CA",$nrec_CA); // recovery size CA
+	defineConstant("v_SS_AK",$ss_AK); // sample size
+	defineConstant("v_NANC_AK",$nanc_AK); // ancestral size
+	defineConstant("v_NU_AK",$nu_AK); // contraction size
+	defineConstant("v_NREC_AK",$nrec_AK); // contraction size
+	defineConstant("v_NCOMBO",$nanc_combo); // pre split time
+	defineConstant("migAK_CA",$migAtoC); // migration to CA from AK
+	defineConstant("migCA_AK",$migCtoA); // migration to AK from CA
 	
 
 	//cat("Exome portion length:"+seqLength+"\n");
@@ -31,12 +90,15 @@ initialize() {
 	// m1 mutation type: neutral *synonymous*
 	initializeMutationType("m1", 0.5, "f", 0.0);
 	// m2 mutation type: missense(?) -- This is from Chris; ask where he got params (Kim et al?)
-	initializeMutationType("m2", v_h, "g",-0.01314833, 0.186); // set H in array (0 for recessive, 0.5 for additive) 
+	initializeMutationType("m2", 0.5, "g",-0.05, 0.5); // set H in array (0 for recessive, 0.5 for additive) 
+	// m3 mutation type: lethal mutations
+	initializeMutationType("m3", 0.0, "f", -1.0);
+	m3.convertToSubstitution = F; 
 	m2.convertToSubstitution = F; // okay cool if you have this, then fixed sites are in the vcf file
 	m1.convertToSubstitution = F; // keeps fixed sites in vcf file (do I want?)
 	// initialize exon: g1, has both neutral (m1) and misense (m2) mutations
 	// syn happens at rate 1, mis at rate 2.31:1 since there are more missense sites (Christian/Chris); note chris names neutral as m2 and missense at m1 so I reversed things here 
-	initializeGenomicElementType("g1", c(m1,m2), c(1.0,2.31)); // 2.31 is the NS:S ratio
+	initializeGenomicElementType("g1", c(m1,m2,m3), c(43.3,95,5)); // this keeps the 2.31:1 NS:S ratio with 5% lethal
 	
 	for (i in 0:(g-1)){
 		initializeGenomicElement(g1, ((i)*geneLength+1), ((i+1)*geneLength));
@@ -58,7 +120,17 @@ initialize() {
 
 
 }
-
+1: fitness(m2) {
+// this is from Deng and Lynch: 
+h = 0.5 * exp(-13*abs(mut.selectionCoeff));
+if (homozygous) {
+    // 20210107: this was Bernard's code to deal with slight excess heterosis in his Plos Genet paper; we don't need this; initial set of revisions was run with this, but then we re-ran without it for final submission --> return ((1.0 + 0.5*mut.selectionCoeff)*(1.0 + 0.5*mut.selectionCoeff));
+    // 20210107: now am calculating homozgyous derived fitness the same as in the other simulations: 
+    return (1.0 + mut.selectionCoeff);
+} else {
+	return (1.0 + mut.selectionCoeff * h);
+}
+}
 // create a population of variable v_NANC individuals
 1 {
 	sim.addSubpop("p1", v_NCOMBO);
@@ -66,7 +138,7 @@ initialize() {
 
 // output generation number so I can track progress
 
-1:50000 late() {
+1:${t} late() {
 	if (sim.generation % 1000 == 0){
 		cat(sim.generation+"\n");
 	}
@@ -74,10 +146,10 @@ initialize() {
 
 //Sample After burn in 
 
-50000 late() {
-	//set up output file
+${t} late() {
+			// set up outfile: 
 	writeFile(paste(c(outdir,"/slim.output.",v_CHUNK,".summary.txt"),sep=""),"replicate,chunk,generation,mutid,type,s,age,originpop,subpop,numhet,numhom,popsizeDIP\n",append=F); // open fresh file
-		//file header
+	//file header
 	//mutation id
 	//mutation type
 	//selection coefficient
@@ -120,20 +192,24 @@ initialize() {
 				type = "m1";
 			} else if (type == m2){
 				type = "m2";
+			} else if (type == m3){
+				type = "m3";
 			}
 			//print results
 		writeFile(paste(c(outdir,"/slim.output.",v_CHUNK,".summary.txt"),sep=""),paste(c(v_REP,v_CHUNK,generation,id,type,s,age,originpop,popID,pnumhet,pnumhom,popsize),sep=","),append=T);
 		}
 	}
-}
+	}
+
 //Burn in 50,000 generations, then split pops. Use CA params for p2 Resize p1 to the AK parameters
-50001  early() {
+$((${t} + 1))  early() {
 	sim.addSubpopSplit("p2",v_NANC_CA,p1);
 	p1.setSubpopulationSize(v_NANC_AK);
+
 }
 
 //after burn in, calculate load every 1000 generations until contraction 
-50001:54001 late() {
+$((${t} + 1)):$((${t} + 1+ ${tdiv})) late() {
 	if (sim.generation % 1000 == 0){
 	//file header
 	//mutation id
@@ -188,14 +264,14 @@ initialize() {
 
 
 //Sample before contraction
-54001 late() {
+$((${t} + 1+ ${tdiv})) late() {
 	p1.outputVCFSample(v_SS_AK, F,filePath=paste(c(outdir,"/slim.output.PreContraction.p1.",v_CHUNK,".vcf"),sep=""));
 	p2.outputVCFSample(v_SS_CA, F,filePath=paste(c(outdir,"/slim.output.PreContraction.p2.",v_CHUNK,".vcf"),sep=""));
 }
 
 
 //after burn in and precontraction, calculate load every 2 generations until endsim  
-54002: late() {
+$((${t} + 2+ ${tdiv})): late() {
 	if (sim.generation % 2 == 0){
 	//file header
 	//mutation id
@@ -251,20 +327,21 @@ initialize() {
 
 //Contract after 4000 generations 
 
-54002 late() {
+$((${t} + 2+ ${tdiv})) late() {
 	p1.setSubpopulationSize(v_NU_AK);
 	p2.setSubpopulationSize(v_NU_CA);
 }
 
+
 //Sample before recovery
-54037 late() {
+$((${t} + 2+ ${tdiv} + ${tcontract})) late() {
 	p1.outputVCFSample(v_SS_AK, F,filePath=paste(c(outdir,"/slim.output.PreRecovery.p1.",v_CHUNK,".vcf"),sep=""));
 	p2.outputVCFSample(v_SS_CA, F,filePath=paste(c(outdir,"/slim.output.PreRecovery.p2.",v_CHUNK,".vcf"),sep=""));
 }
 
 //Recover populatoin sizes after (35/25) generations. Tcontract is 35, but CA started 10 generations later
 
-54038 late() {
+$((${t} + 3+ ${tdiv} + ${tcontract})) late() {
 	p1.setSubpopulationSize(v_NREC_AK);
 	p2.setSubpopulationSize(v_NREC_CA);
 	}
@@ -272,21 +349,25 @@ initialize() {
 //Present Day is here! Should be around 14 gens later
 
 //Sample before setting up migration rates
-54055 late() {
+$((${t} + 3+ ${tdiv} + ${tcontract}+ ${trecovery})) late() {
 	p1.outputVCFSample(v_SS_AK, F,filePath=paste(c(outdir,"/slim.output.PreMigration.p1.",v_CHUNK,".vcf"),sep=""));
 	p2.outputVCFSample(v_SS_CA, F,filePath=paste(c(outdir,"/slim.output.PreMigration.p2.",v_CHUNK,".vcf"),sep=""));
 }
-//54056 late() {
-		//p1.setMigrationRates(c(p2), c(migCA_AK));
-		//p2.setMigrationRates(c(p1), c(migAK_CA));
-		//}
+$((${t} + 4 + ${tdiv} + ${tcontract}+ ${trecovery})) late() {
+		p1.setMigrationRates(c(p2), c(migCA_AK));
+		p2.setMigrationRates(c(p1), c(migAK_CA));
+		}
 //Let the simulation run into the future, then sample at the ends
-54106 late() {
+$((${t} + 4+ ${tdiv} + ${tcontract}+ ${trecovery}+${tfuture})) late() {
 	p1.outputVCFSample(v_SS_AK, F,filePath=paste(c(outdir,"/slim.output.EndSimulation.p1.",v_CHUNK,".vcf"),sep=""));
 	p2.outputVCFSample(v_SS_CA, F,filePath=paste(c(outdir,"/slim.output.EndSimulation.p2.",v_CHUNK,".vcf"),sep=""));
 }
 
-54107 late() 
+$((${t} + 5 + ${tdiv} + ${tcontract}+ ${trecovery}+${tfuture})) late() 
 {
 sim.simulationFinished();
 } 
+EOM
+
+
+
