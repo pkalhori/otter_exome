28bb52ebb72ac85e8ff47d411130e28fa09fcb2a
pkalhori <pkalhori@g.ucla.edu>
2021-10-03 14:17:28 -0700

    updates

--- SLIM/R_scripts/Linux_Format/load_calcs_1D_ExcludeFixedBurninVariants_Kardos.R
+++ SLIM/R_scripts/Linux_Format/load_calcs_1D_ExcludeFixedBurninVariants_Kardos.R
@@ -5,7 +5,6 @@ require(ggplot2)
 #library(tidyverse)
 library(dplyr)
 todaysdate=format(Sys.Date(),format="%Y%m%d")
-#data.dir="C:\\Users\\poone\\OneDrive\\Documents\\Otter_Exome_Project\\SLIM_results\\"
 data.dir="/u/scratch/p/pkalhori/slim/concattedSummaries/"
 outdir="/u/scratch/p/pkalhori/slim/R_load_calc/"
 #plot.dir="/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/analysisResults/slim/poonehSimulations/loadCalcs/"
@@ -15,7 +14,9 @@ outdir="/u/scratch/p/pkalhori/slim/R_load_calc/"
 #simdates=c(20190424,20190607)
 # skipping AL "AL/1D.2Epoch.1.5Mb.cds/20190424/" and CA etc -- add those in next 
 
-popModDates=c("AK/1D.5Epoch/20211001/") # AK and AL have dadi parameters, genericPop has parameters based on AK MLE grid that is fur-trade relevant. ### need to come up with better classification system for this. 
+popModDates=c("CA/1D.3Epoch.LongerRecovery/20210927/")
+
+#popModDates=c("CA/1D.5Epoch/20210927/") # AK and AL have dadi parameters, genericPop has parameters based on AK MLE grid that is fur-trade relevant. ### need to come up with better classification system for this. 
 #/u/scratch/p/pkalhori/slim/concattedSummaries/CA/1D.3Epoch.LongerRecovery/20210927/Kardos_hs
 #popModDates=c("CA_AK/2D.3Epoch.NoTranslocation/20210127/", "CA_AK/2D.3Epoch.Translocation.1perGen/20210127/","CA_AK/2D.3Epoch.Translocation.25perGen/20210127/", "CA_AK/2D.3Epoch.Translocation.25for2Gen/20210127/")
 
@@ -36,10 +37,9 @@ for(popModDate in popModDates){
     print(rep)
     # check if rep exists (some have random hoffman failures)
     infile=paste(data.dir,popModDate,"Kardos_hs/replicate_",rep,".slim.output.allConcatted.summary.txt.gz",sep="")
-	print(infile)
+    print(infile)
     if(file.exists(infile)){
       input = read.table(infile,sep=",",header=T)
-      
       print("file exists")
       # want to exclude sites that are at frequency 1 *prior* to the bottleneck
       pop= unlist(lapply(strsplit(popModDate,"/"),"[",1))
